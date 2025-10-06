d205e0995189c2fc755b5ab2baf39cc85a9a5447
Victor Hugo <vhclaudiogomes@gmail.com>
2025-10-06 10:05:32 -0300

    filters routes added

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,13 +15,17 @@ import (
 func SetupRouter(ctx context.Context, db *gorm.DB, redis *redis.Client) *gin.Engine {
 	router := gin.Default()
 
+	// repos
 	siteRepo := repository2.NewSiteRepo(db)
 	siteStatusRepo := repository2.NewSiteStatusRepo(db)
 	userRepo := repository2.NewUserRepo(db)
 
+	// handlers
 	siteHandler := handlers2.NewSiteHandler(siteRepo, siteStatusRepo, ctx)
 	userHandler := handlers2.NewUserHandler(userRepo)
+	siteStatusHandler := handlers2.NewSiteStatusHandler(siteStatusRepo)
 
+	// cronjobs
 	monitor.StartMonitoring(ctx, siteRepo, siteStatusRepo)
 
 	cronManager := cronjobs.NewJobManager(ctx)
@@ -32,13 +36,25 @@ func SetupRouter(ctx context.Context, db *gorm.DB, redis *redis.Client) *gin.Eng
 	auth := router.Group("/")
 	auth.Use(middlewares.MiddlewareJWT())
 	{
+		// rotas basicas dos sites
 		auth.POST("/sites", siteHandler.CreateSite)
 		auth.GET("/sites", siteHandler.GetAllSitesByUser)
 		auth.GET("/sites/:id", siteHandler.GetSiteById)
 		auth.PUT("/sites/:id", siteHandler.UpdateSite)
 		auth.DELETE("/sites/:id", siteHandler.DeleteSite)
+
+		// rotas filtros
+		status := auth.Group("/sites/:id/status")
+		{
+			status.GET("", siteStatusHandler.GetAllSiteStatusBySiteIdPaginated)
+			status.GET("/date", siteStatusHandler.GetAllSiteStatusByDatePaginated)
+			status.GET("/filter", siteStatusHandler.GetAllSiteStatusByStatusPaginated)
+			status.GET("/last", siteStatusHandler.GetLastSiteStatus)
+			status.GET("/first", siteStatusHandler.GetFirstSiteStatus)
+		}
 	}
 
+	//rotas publicas
 	router.POST("/register", userHandler.RegisterUser)
 	router.POST("/login", userHandler.Login)
 
